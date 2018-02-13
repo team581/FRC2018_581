f7f41bb7c78926d9f59926076c25e81166e78cd7
Jonah Snider <me@jonahsnider.ninja>
2018-02-12 18:42:58 -0800

    Add explanation to the top of the file.

--- src/arduino/pixy-i2c.ino
+++ src/arduino/pixy-i2c.ino
@@ -1,5 +1,11 @@
 // Copyright Jonah Snider 2018
 
+/*
+This sends the x coordinate and the width of a detected object via I2C.
+It sends it in this format: "x<x-coord>w<width>"
+For example: "x78w214"
+*/
+
 #include <SPI.h>
 #include <Pixy.h>
 #include <Wire.h>
