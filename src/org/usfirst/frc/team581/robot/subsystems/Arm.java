5c599122181df6c28475c1397a0f99e491662dcf
Stephen Wragg <stephen.wragg@gmail.com>
2018-02-14 10:32:30 -0800

    All of Maanasa and Max's changes.  Arm moves but no encoder data.

--- src/org/usfirst/frc/team581/robot/subsystems/Arm.java
+++ src/org/usfirst/frc/team581/robot/subsystems/Arm.java
@@ -1,27 +1,60 @@
 package org.usfirst.frc.team581.robot.subsystems;
 
 
+import org.usfirst.frc.team581.robot.Robot;
 import org.usfirst.frc.team581.robot.commands.ArmDrive;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Arm extends Subsystem{
 	public DifferentialDrive armDrive;
-	public WPI_TalonSRX tal0 = new WPI_TalonSRX(0);
-	public WPI_TalonSRX tal1 = new WPI_TalonSRX(1);
+	public TalonSRX tal0 = new TalonSRX(0);
+	public TalonSRX tal1 = new TalonSRX(1);
+	public int pulseWidthPos;
+	//public SpeedControllerGroup talonGroup;
+	//public PIDController pidController;
+	//public ArmEncoder encoder;
+	//public double zero;
 
 	public Arm() {
-		armDrive = new DifferentialDrive(tal0, tal1);
+		//armDrive = new DifferentialDrive(tal0, tal1);
+		tal1.follow(tal0);
+		tal0.setInverted(true);
+		tal0.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute,0,0);
+		tal0.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
+		
+		double targetPositionRotations = tal0.getSelectedSensorPosition(0);
+		tal0.set(ControlMode.Position, targetPositionRotations);
 	}
+	
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 		setDefaultCommand(new ArmDrive());
 	}
+	double velocity;
+	public void testDrive () {
+		if (Robot.oi.joy1.getRawButton(1)) {
+			tal0.set(ControlMode.PercentOutput, 0.25); /* 25 % output */
+			velocity = tal0.getSelectedSensorVelocity(0);
+		}
+		else 
+			tal0.set(ControlMode.PercentOutput, 0.0);
+		
+		SmartDashboard.putString("DB/String 3", "" + velocity);
+		
+	}
+	
 	public void driveArm(double y) {
-		armDrive.tankDrive(y, y);
+		//armDrive.tankDrive(y, y);
+		pulseWidthPos = tal0.getSelectedSensorPosition(0);
+		SmartDashboard.putString("DB/String 3", "" + pulseWidthPos);
 	}
 
 }
--- src/org/usfirst/frc/team581/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team581/robot/subsystems/Drive.java
@@ -1,14 +1,19 @@
 package org.usfirst.frc.team581.robot.subsystems;
 
+import org.usfirst.frc.team581.robot.Robot;
 import org.usfirst.frc.team581.robot.RobotMap;
 import org.usfirst.frc.team581.robot.commands.DriverDrive;
 
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.tables.ITable;
 
 public class Drive extends Subsystem{
 	
@@ -17,26 +22,53 @@ public class Drive extends Subsystem{
 	public Encoder encL;
 	public Spark m_left = new Spark(RobotMap.leftMotor);
 	public Spark m_right = new Spark(RobotMap.rightMotor);
-	
+	public double INCHES_PER_PULSE = 1/18.9;
 	
 	public Drive() {
 		mydrive = new DifferentialDrive(m_left, m_right);
 		encR = new Encoder(0,1,false, Encoder.EncodingType.k4X);
-		encL = new Encoder(2,3,false, Encoder.EncodingType.k4X); 
-		encR.setMaxPeriod(0.1);
-		encL.setMaxPeriod(0.1);
-		encR.setMinRate(10);
-		encL.setMinRate(10);
-		//encR.reset();
-		//encL.reset();
-		encR.setDistancePerPulse(5);
-		encL.setDistancePerPulse(5);
+		encL = new Encoder(2,3,true, Encoder.EncodingType.k4X); 
+		//encL.setPIDSourceType(PIDSourceType.kDisplacement);
+       //encR.setPIDSourceType(PIDSourceType.kDisplacement);
+		//encR.setMaxPeriod(0.1);
+		//encL.setMaxPeriod(0.1);
+		//encR.setMinRate(10);
+		//encL.setMinRate(10);
+		encR.reset();
+		encL.reset();
+		encR.setDistancePerPulse(INCHES_PER_PULSE);
+		encL.setDistancePerPulse(INCHES_PER_PULSE);
 	}
 
 	@Override
 	protected void initDefaultCommand() {
       setDefaultCommand(new DriverDrive());		
 	}
+	/*
+	public ITable getTable() {
+
+        NetworkTable driveTable;
+
+        driveTable = NetworkTable.getTable("Drivetrain");
+
+        driveTable.putNumber("Left PWM", leftMotor.get());
+
+        driveTable.putNumber("Right PWM", rightMotor.get());
+
+        driveTable.putNumber("Left Speed (inch per second)", leftEncoder.getRate());
+
+        driveTable.putNumber("Right Speed (inch per second)", rightEncoder.getRate());
+
+        driveTable.putNumber("Left Distance (inches)", leftEncoder.getDistance());
+
+        driveTable.putNumber("Right Distance (inches)", rightEncoder.getDistance());
+
+        SmartDashboard.putData("Drivetrain PID Controller", arcadeController);
+
+        return driveTable;
+
+    }
+    */
 	
     final double startMoving = 0.45;
 	
@@ -62,8 +94,14 @@ public class Drive extends Subsystem{
 		double left = x + y/2;
 		return left;
 	}
+	public double leftPulses, rightPulses;
 	public void driveWithGamepad(double xspeed, double yspeed) {
+		leftPulses = Robot.drive.encR.get();
+		rightPulses = Robot.drive.encL.get();
 		mydrive.tankDrive(smartL(xspeed, yspeed), smartR(xspeed, yspeed));
+		SmartDashboard.putString("DB/String 0", "" + leftPulses);
+		SmartDashboard.putString("DB/String 1", "" + rightPulses);
+		
 	}
 	public void drive(double xspeed, double yspeed) {
 		mydrive.tankDrive(xspeed, yspeed);
