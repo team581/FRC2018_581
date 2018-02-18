ac5afcd368133c2e709cef5f6eacb291bfb7f9f9
Blazing Bulldogs <Blazing Bulldogs@10.221.245.89>
2018-02-18 12:24:13 -0800

    Merge from GitHub.

--- src/org/usfirst/frc/team581/robot/commands/ArmAngle.java
+++ src/org/usfirst/frc/team581/robot/commands/ArmAngle.java
@@ -4,15 +4,16 @@ import org.usfirst.frc.team581.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-public class ArmDrive extends Command{
+public class ArmAngle extends Command{
 
-	public ArmDrive(){
+	public ArmAngle() {
 		// TODO Auto-generated constructor stub
 		requires(Robot.arm);
 	}
 	protected void execute() {
-		Robot.arm.driveArm();
+		Robot.arm.setAngle(90);
 	}
+	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
 		return false;
---
+++ src/org/usfirst/frc/team581/robot/sensors/UltraSonic.java
@@ -0,0 +1,78 @@
+package org.usfirst.frc.team581.robot.sensors;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+public class UltraSonic extends AnalogInput{
+
+	 private static final int DEFAULT_SAMPLES = 64;
+	    private static final double INCHES_PER_VOLT = 42.56;
+	    private static final double OPERATING_RANGE = 150.0;
+	    
+	    /**
+	     * Creates an analog ultrasonic sensor.
+	     * @param port Analog port of the sensor.
+	     */
+	    public UltraSonic (int port) {
+	        super(port);
+	    }
+
+	    /**
+	     * Gets the current analog value of the sensor using 64 samples.
+	     * @return The current analog value of the sensor.
+	     */
+	    public double getAnalog () {
+	        return this.getAnalog(DEFAULT_SAMPLES);
+	    }
+
+	    /**
+	     * Gets the current analog value of the sensor.
+	     * @param samples Number of samples to take.
+	     * @return The current analog value of the sensor.
+	     */
+	    public double getAnalog (int samples) {
+	        double total = 0;
+	        for (int f = 0; f < samples; f++) {
+	            total += super.getValue();
+	        }
+	        
+	        total /= samples;
+	        return total;
+	    }
+
+	    /**
+	     * Gets the current voltage of the sensor using 64 samples.
+	     * @return The current voltage of the sensor.
+	     */
+	    public double getVoltage () {
+	        return this.getVoltage(DEFAULT_SAMPLES);
+	    }
+
+	    /**
+	     * Gets the current voltage of the sensor.
+	     * @param samples Number of samples to take.
+	     * @return The current voltage of the sensor.
+	     */
+	    public double getVoltage (int samples) {
+	        double total = 0;
+	        for (int f = 0; f < samples; f++) {
+	            total += super.getVoltage();
+	        }
+	        
+	        total /= samples;
+	        return total;
+	    }
+
+	    public double getDistance() {
+	        return this.getVoltage() * INCHES_PER_VOLT;
+	    }
+
+	    public double getDistance(int samples) {
+	        return this.getVoltage(samples) * INCHES_PER_VOLT;
+	    }
+	    
+	    public boolean inRange() {
+	        return this.getDistance(1) < OPERATING_RANGE;
+	    }
+
+
+}
